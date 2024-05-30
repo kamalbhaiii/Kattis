fadf7f1e53069344fd83fbfab995ad18a3c05b6c
kamalbhaiii <kamal5201ks@gmail.com>
2024-05-30 10:21:17 +0200

    Matrix Inverse

---
+++ Matrix Inverse/matrix.c
@@ -0,0 +1,15 @@
+//  Problem name: Matrix Inverse
+//  Solved by: Kamal 
+
+#include <stdio.h>
+int main()
+{
+    int a, b, c, d, test_case = 1;
+    while (scanf("%d %d %d %d", &a, &b, &c, &d) == 4)
+    {
+        printf("Case %d:\n", test_case++);
+        int det = a*d - b*c;
+        printf("%d %d\n%d %d\n", d / det, -b / det, -c / det, a / det);
+    }
+    return 0;
+}
