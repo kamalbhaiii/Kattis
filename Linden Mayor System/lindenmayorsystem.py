f84727008f1c28836e8678cacee3e954d75ff18a
kamalbhaiii <kamal5201ks@gmail.com>
2024-06-02 10:25:59 +0200

    Linden Mayor System

---
+++ Linden Mayor System/lindenmayorsystem.py
@@ -0,0 +1,16 @@
+# Problem name: Linden Mayor System
+# Solved by: Kamal
+
+def main(getline = input):
+    n,m = map(int, getline().split())
+    rules = [chr(65+i) for i in range(26)]
+    for _ in range(n):
+        a,b = getline().split(' -> ')
+        rules[ord(a) - 65] = b
+    curr = getline()
+    for _ in range(m):
+        curr = ''.join((rules[ord(c) - 65] for c in curr))
+    print(curr)
+
+if __name__ == "__main__":
+    main()
